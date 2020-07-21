aa69d794eb1b3e7f984268329d0417b7b2d07e03
hybrid_blue <aaronifill@gmail.com>
2020-07-21 19:19:56 +0100

    :sparkles: Update index for testing features

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import grid from './app/val-engine.js'
+import grid from './app/val-engine.js';
 
 const iso = new grid({
   root: '#root',
-  size: {width: 900, height: 900},
-  // tileset: './resource/tiles.png'
+  tileset: {
+    'grass': '/res/grass.png',
+    'tree': '/res/tree.png',
+    'pyramid': '/res/pyramid.png',
+    'ball': '/res/ball.png'
+  },
+  zHeight: 10,
+  tileSize: 120,
+  tilesX: 7,
+  tilesY: 7
 })
 
 iso.onClickLeft((e, tile) => {
-  iso.editTile('/src/resources/pyramid.png', tile, e)
+
+  let tiles = document.querySelectorAll('[name="tiles"]');
+  let tileset = document.querySelector('#tile-list');
+  var img;
+
+  tiles.forEach(tile => {
+    if(tile.checked){
+      img = tile.dataset.tile;
+    }
+  })
+
+  iso.editTile(img, tile, e)
   let ctxMenu = document.querySelector('#context-menu');
   if(ctxMenu) ctxMenu.parentNode.removeChild(ctxMenu);
 });
@@ -41,9 +60,10 @@ iso.onClickRight((e, tile) => {
 
   let list = [
     {name: 'Remove Tile', id: 'remove-tile'},
-    {name: 'Remove All', id: 'remove-all'},
     {name: 'Increase Z level', id: 'raise-z-index'},
-    {name: 'Decrease Z level', id: 'decrease-z-index'}
+    {name: 'Decrease Z level', id: 'decrease-z-index'},
+    {name: 'Remove All', id: 'remove-all'},
+    {name: 'Flatten map', id: 'all-z-index-zero'}
   ]
 
   for(let i=0;i<list.length;i++){
@@ -61,29 +81,62 @@ iso.onClickRight((e, tile) => {
       break;
       case 1:
         ctxList.querySelectorAll('li')[1].addEventListener('click', () => {
-          iso.clearAll();
+          iso.tileLevel('increase', tile);
           if(ctxMenu) ctxMenu.parentNode.removeChild(ctxMenu);
         })
       break;
       case 2:
         ctxList.querySelectorAll('li')[2].addEventListener('click', () => {
-          iso.tileLevel('increase', tile);
+          iso.tileLevel('decrease', tile);
           if(ctxMenu) ctxMenu.parentNode.removeChild(ctxMenu);
         })
       break;
       case 3:
         ctxList.querySelectorAll('li')[3].addEventListener('click', () => {
-          iso.tileLevel('decrease', tile);
+          iso.clearAll();
+          if(ctxMenu) ctxMenu.parentNode.removeChild(ctxMenu);
+        })
+      break;
+      case 4:
+        ctxList.querySelectorAll('li')[4].addEventListener('click', () => {
+          iso.levelZero();
           if(ctxMenu) ctxMenu.parentNode.removeChild(ctxMenu);
         })
       break;
     }
 
-
-
   }
 
 });
+
+
+(function(){
+
+  var elm = document.createElement('div');
+  elm.setAttribute('id', 'tile-list')
+
+  Object.keys(iso.tileset).forEach((item, i) => {
+    let label = document.createElement('label');
+    var radio = document.createElement('input');
+    radio.setAttribute('type', 'radio');
+    radio.setAttribute('name', 'tiles');
+    radio.setAttribute('id', `tile-${item}`);
+    radio.setAttribute('data-tile', iso.tileset[item]);
+
+    if(i === 0) radio.checked = true;
+
+    label.innerHTML = item;
+
+    label.appendChild(radio)
+
+    elm.appendChild(label)
+  })
+
+  root.appendChild(elm);
+
+  let tiles = document.querySelectorAll('[name="tiles"]');
+
+})();
 
 
 iso.draw();
